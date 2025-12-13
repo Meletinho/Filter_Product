1c28e4d1741e7fefd6ad6eaebf5a872d33ad8fc2
Meletinho <vinipitta01@gmail.com>
2025-12-13 01:31:16 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/com/saas/filtro/FilterProductApplication.java
@@ -0,0 +1,11 @@
+package com.saas.filtro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilterProductApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FilterProductApplication.class, args);
+    }
+}
